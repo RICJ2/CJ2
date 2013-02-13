6aa9b5f2ed9d13a6eccd6e01b64446126dfb1eb3
john dixon <jedixon6565@gmail.com>
2013-02-13 08:37:34 -0500

    Changes to try and set login screen as the startup....

--- NetBeansProjects/LoginScreen/src/loginscreen/LoginScreen.java
+++ NetBeansProjects/LoginScreen/src/loginscreen/LoginScreen.java
@@ -9,18 +9,11 @@ package loginscreen;
  * @author jroberti
  */
 public class LoginScreen {
-
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         // TODO code application logic here
-    java.awt.EventQueue.invokeLater(new Runnable() {
 
-            public void run() {
-                new LoginScreenForm().setVisible(true);
-            }
-        });
 }
-
 }
