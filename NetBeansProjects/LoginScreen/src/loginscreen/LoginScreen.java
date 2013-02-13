f5f284c5e97991d3ec703045c7612152ccfe4ff7
john dixon <jedixon6565@gmail.com>
2013-02-13 08:22:37 -0500

    set LoginScreen to call LoginScreenForm as main form to open....

--- NetBeansProjects/LoginScreen/src/loginscreen/LoginScreen.java
+++ NetBeansProjects/LoginScreen/src/loginscreen/LoginScreen.java
@@ -15,5 +15,12 @@ public class LoginScreen {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+    java.awt.EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+                new LoginScreenForm().setVisible(true);
+            }
+        });
+}
+
 }
