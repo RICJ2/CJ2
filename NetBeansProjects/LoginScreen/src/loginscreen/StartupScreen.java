f19262744a036f4115d10d8e12e8f19cbcfc37c4
john dixon <jedixon6565@gmail.com>
2013-02-13 01:50:27 -0500

    uncommented the package LoginScreen statement

--- NetBeansProjects/LoginScreen/src/loginscreen/StartupScreen.java
+++ NetBeansProjects/LoginScreen/src/loginscreen/StartupScreen.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-//package loginscreen;
+package loginscreen;
 
 import java.sql.*;
 import javax.swing.*;
