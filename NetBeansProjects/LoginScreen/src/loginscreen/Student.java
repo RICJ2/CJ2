358dcb5d76619620fc805320d4dda8340fae329a
john dixon <jedixon6565@gmail.com>
2013-03-26 19:19:36 -0400

    modified the comments section at the top for commit purposes Signed-off-by:john dixon <jedixon6565@gmail.com>

--- NetBeansProjects/LoginScreen/src/loginscreen/Student.java
+++ NetBeansProjects/LoginScreen/src/loginscreen/Student.java
@@ -1,6 +1,6 @@
 /*
  * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor. this is a test part of a commit
  */
 package loginscreen;
 
