21b89caf7942d2c6525e8ae47840cbcd673b7e0f
john dixon <jedixon6565@gmail.com>
2013-03-26 01:55:21 -0400

    Minor changes to student by removing the initializing of result to "" this is not needed for this to function correctly. Also added Connection as a static item as a main declaration. It does not need to be named in each method each time. Declared once and used many times. Signed-off-by:john dixon <jedixon6565@gmail.com>

--- NetBeansProjects/LoginScreen/src/loginscreen/Student.java
+++ NetBeansProjects/LoginScreen/src/loginscreen/Student.java
@@ -19,6 +19,7 @@ import javax.swing.JOptionPane;
 class Student{
     static PreparedStatement prestmt;
     static ResultSet rs;
+    static Connection c;
 
     public Student()
     {}
@@ -29,7 +30,7 @@ class Student{
     }
 
     public static String getFirstName(String loginN, Connection c){
-        String result = "";
+        String result;
         String user_query = "select * from users where login_Name = ?";
         try {
             prestmt = c.prepareStatement(user_query);
@@ -44,7 +45,7 @@ class Student{
     }
 
     public static String getPassword(String loginN, Connection c){
-        String result = "";
+        String result;
         String user_query = "select * from users where login_Name = ?";
         try {
             prestmt = c.prepareStatement(user_query);
@@ -72,7 +73,7 @@ class Student{
     }
 
     public static String getLastName(String loginN, Connection c){
-        String result = "";
+        String result;
         String user_query = "select * from users where login_Name = ?";
         try {
             prestmt = c.prepareStatement(user_query);
@@ -87,7 +88,7 @@ class Student{
     }
 
     public static String getMajor(String loginN, Connection c){
-        String result = "";
+        String result;
         String user_query = "select * from users where login_Name = ?";
         try {
             prestmt = c.prepareStatement(user_query);
@@ -101,12 +102,12 @@ class Student{
         return result;
     }
 
-	public static void createAccount(String firstN, String lastN, String major, String semester, String year, String email, String username, String password, String passval, Connection conn){
+	public static void createAccount(String firstN, String lastN, String major, String semester, String year, String email, String username, String password, String passval, Connection c){
 //needed to have String passval added due to the expectation that there are nine(9) parameters but was only passing over 8 and with the select query embedded in the insert it made the problem stand out because of the uniqueness of the insert query. So on the CreateAccount java code I added in the confirmTextField.getText() and I am passing that to the passval. So to sum it up, we can't pass 'password' twice as a unique parameter value we needed to have something unique so I passed the actual password validator field.
             String user_query = "INSERT INTO users (f_name, l_name, login_name, pword, pword_val,major, sem_start, gr_date, s_email)Values(?,?,?,?,?,(SELECT iddegree_prog from degree_prog where degree_desc = ?), ?,?,?)";
        // String user_query = "INSERT INTO users (f_Name, l_Name, login_Name, pword, pword_val, major, sem_start, gr_date, s_email)VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
         try {
-            prestmt = conn.prepareStatement(user_query);
+            prestmt = c.prepareStatement(user_query);
             prestmt.setString(1, firstN);
             prestmt.setString(2, lastN);
             prestmt.setString(3, username);
@@ -122,5 +123,5 @@ class Student{
             JOptionPane.showMessageDialog(null, e);
         }
 
-	}
+        }
 }
