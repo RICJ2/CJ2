d3b75aca3351b15f219c6a662cd36443d1003eb3
Jonathan Roberti <jroberti_5340@email.ric.edu>
2013-04-09 16:28:41 -0400

    Moved the DragNDrop to a jframe

---
+++ NetBeansProjects/LoginScreen/src/loginscreen/DragNDrop.java
@@ -0,0 +1,259 @@
+package loginscreen;
+import java.sql.Connection;
+import javax.swing.JTree;
+import javax.swing.DefaultListModel;
+import javax.swing.JComponent;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import javax.swing.TransferHandler;
+import javax.swing.DropMode;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeSelectionModel;
+import java.awt.datatransfer.*;
+
+import javax.swing.*;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+/**
+ *
+ * @author jroberti
+ */
+public class DragNdrop extends javax.swing.JFrame {   
+     Mediator mediator;
+     Connection conn = null;
+     ResultSet rs = null;
+     PreparedStatement pst = null;
+
+    public DragNdrop(Mediator m, Connection c) {
+        mediator = m;
+        conn = c;
+        initComponents();
+        list.setDropMode(DropMode.ON_OR_INSERT);
+    }
+   private void displayDropLocation(final String string) {
+        SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    JOptionPane.showMessageDialog(null, string);
+                }
+            });
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tree = new javax.swing.JTree();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        jList2 = new javax.swing.JList();
+        jPanel1 = new javax.swing.JPanel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        list = new javax.swing.JList();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        //Create a tree.
+        DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode("Major");
+        DefaultMutableTreeNode folder = new DefaultMutableTreeNode("CSCI");
+        rootNode.add(folder);
+        try{
+            String sql = "select * from courses WHERE course_type = 'CSCI'";
+            pst = conn.prepareStatement(sql);
+            rs = pst.executeQuery();
+            while (rs.next()){
+                String cid = rs.getString("course_id");
+                DefaultMutableTreeNode majorCoruses = new DefaultMutableTreeNode(cid);
+                folder.add(majorCoruses);
+
+            }
+        } catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+        DefaultMutableTreeNode subFolder = new DefaultMutableTreeNode("Math");
+        rootNode.add(subFolder);
+        try{
+            String sql = "select * from courses WHERE course_type = 'MATH'";
+            pst = conn.prepareStatement(sql);
+            rs = pst.executeQuery();
+            while (rs.next()){
+                String cid = rs.getString("course_id");
+                DefaultMutableTreeNode majorCoruses = new DefaultMutableTreeNode(cid);
+                subFolder.add(majorCoruses);
+
+            }
+        } catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+        //DefaultMutableTreeNode anya = new DefaultMutableTreeNode("Anya");
+        //folder.add(anya);
+        //folder.add(new DefaultMutableTreeNode("Bongo"));
+
+        //anya.add(new DefaultMutableTreeNode("Winky"));
+        DefaultTreeModel model = new DefaultTreeModel(rootNode);
+        tree = new JTree(model);
+        tree.getSelectionModel().setSelectionMode
+        (TreeSelectionModel.DISCONTIGUOUS_TREE_SELECTION);
+        tree.setDragEnabled(true);
+        jScrollPane1.setViewportView(tree);
+
+        jList2.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane4.setViewportView(jList2);
+
+        jPanel1.setBackground(new java.awt.Color(255, 51, 51));
+
+        DefaultListModel listModel = new DefaultListModel();
+        listModel.addElement("Add and drop courses");
+        listModel.addElement(" ");
+        listModel.addElement(" ");
+        listModel.addElement(" ");
+        listModel.addElement("");
+        listModel.addElement(" ");
+        list.setToolTipText("");
+        list = new JList(listModel);
+        //list.setVisibleRowCount(-1);
+        list.getSelectionModel().setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+
+        list.setTransferHandler(new TransferHandler() {
+
+            public boolean canImport(TransferHandler.TransferSupport info) {
+                // we only import Strings
+                if (!info.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                    return false;
+                }
+
+                JList.DropLocation dl = (JList.DropLocation)info.getDropLocation();
+                if (dl.getIndex() == -1) {
+                    return false;
+                }
+                return true;
+            }
+
+            public boolean importData(TransferHandler.TransferSupport info) {
+                if (!info.isDrop()) {
+                    return false;
+                }
+
+                // Check for String flavor
+                if (!info.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                    displayDropLocation("List doesn't accept a drop of this type.");
+                    return false;
+                }
+
+                JList.DropLocation dl = (JList.DropLocation)info.getDropLocation();
+                DefaultListModel listModel = (DefaultListModel)list.getModel();
+                int index = dl.getIndex();
+                boolean insert = dl.isInsert();
+                // Get the current string under the drop.
+                String value = (String)listModel.getElementAt(index);
+
+                // Get the string that is being dropped.
+                Transferable t = info.getTransferable();
+                String data;
+                try {
+                    data = (String)t.getTransferData(DataFlavor.stringFlavor);
+                }
+                catch (Exception e) { return false; }
+
+                // Perform the actual import.
+                if (insert) {
+                    listModel.add(index, data);
+                    tree.DefaultMutableTreeNode(index);
+                } else if( list = 7){
+                    JOptionPane.showMessageDialog(null, "You cannot take more then 6 courses");
+                }
+                else {
+                    listModel.set(index, data);
+                }
+                return true;
+
+                //return false;
+            }
+
+            public int getSourceActions(JComponent c) {
+                return MOVE;
+            }
+
+            protected Transferable createTransferable(JComponent c) {
+                JList list = (JList)c;
+                Object[] values = list.getSelectedValues();
+
+                StringBuffer buff = new StringBuffer();
+
+                for (int i = 0; i < values.length; i++) {
+                    Object val = values[i];
+                    buff.append(val == null ? "" : val.toString());
+                    if (i != values.length - 1) {
+                        buff.append("\n");
+                    }
+                }
+                return new StringSelection(buff.toString());
+            }
+        });
+        jScrollPane3.setViewportView(list);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(108, 108, 108)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 343, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(325, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 167, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 203, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(163, 163, 163)
+                        .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(109, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(118, 118, 118)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(114, 114, 114)
+                        .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 164, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+   
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JList jList2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
+    private javax.swing.JList list;
+    private javax.swing.JTree tree;
+    // End of variables declaration//GEN-END:variables
+}
