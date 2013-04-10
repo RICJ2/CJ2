99f96fda9cafbe6220c34e3af5442ab81bf0520d
john dixon <jedixon6565@gmail.com>
2013-04-09 20:43:10 -0400

    ok...we are back to using the original schema which is savianconsultants_com_2 and had to modify for a new login name but not a issue other than an update. Signed-off-by:john dixon <jedixon6565@gmail.com>

--- NetBeansProjects/LoginScreen/src/loginscreen/DataB.java
+++ NetBeansProjects/LoginScreen/src/loginscreen/DataB.java
@@ -26,7 +26,7 @@ public class DataB {
 
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://www.savianconsultants.com:3306/savianconsultants_com_3","a0000a6f_1","century4last" );
+            conn = DriverManager.getConnection("jdbc:mysql://www.savianconsultants.com:3306/savianconsultants_com_2","a0000a6f","century4last" );
             //JOptionPane.showMessageDialog(null, "Connection Established");
             return conn;
         }catch (Exception e){
